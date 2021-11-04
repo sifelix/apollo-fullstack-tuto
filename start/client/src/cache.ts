64e2a85696f5edf88a80227121cbba56628c016e
sife <felixv@sicara.com>
2021-11-04 18:20:40 +0100

    Add query policies for server side fields

--- start/client/src/cache.ts
+++ start/client/src/cache.ts
@@ -4,6 +4,16 @@ export const cache: InMemoryCache = new InMemoryCache({
   typePolicies: {
     Query: {
       fields: {
+        isLoggedIn: {
+          read() {
+            return isLoggedInVar();
+          },
+        },
+        cartItems: {
+          read() {
+            return cartItemsVar();
+          },
+        },
         launches: {
           keyArgs: false,
           merge(existing, incoming) {
